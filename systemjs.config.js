788727b5434ba9726333ac2010a5066b7985df49
Thomas <thomas.rundle@conduitlearning.com>
2016-09-27 19:24:55 -0500

    Replaced with angular quickstart config

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,24 +1,43 @@
-System.config({
-    transpiler: 'typescript',
-    typescriptOptions: {emitDecoratorMetadata: true},
-    map: {
-        'app' : 'app',
-        'rxjs': 'node_modules/rxjs',
-        '@angular/core' : 'node_modules/@angular/core',
-        '@angular/common' : 'node_modules/@angular/common',
-        '@angular/compiler' : 'node_modules/@angular/compiler',
-        '@angular/router' : 'node_modules/@angular/router',
-        '@angular/platform-browser' : 'node_modules/@angular/platform-browser',
-        '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic'
+/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
     },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // other libraries
+      'rxjs':                      'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
     packages: {
-        'app' : {main: 'main.ts', defaultExtension: 'ts'},
-        'rxjs' : {main: 'index.js'},
-        '@angular/core' : {main: 'index.js'},
-        '@angular/common' : {main: 'index.js'},
-        '@angular/compiler' : {main: 'index.js'},
-        '@angular/router' : {main: 'index.js'},
-        '@angular/platform-browser' : {main: 'index.js'},
-        '@angular/platform-browser-dynamic': {main: 'index.js'}
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      'angular-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
+      }
     }
-});
+  });
+})(this);
